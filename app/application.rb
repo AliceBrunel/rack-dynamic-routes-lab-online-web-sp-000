473c455a244121d844510c14683568bc210aa37e
Alice Brunel <alc.brunel@gmail.com>
2019-09-22 09:56:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,15 @@
 class Application
- 
+
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+ 
+  if req.path.match(/items/)
+    
+    item_name = req.path.split("/items/").last 
+    item = @@items.find{|i| i.
  
   end
  
