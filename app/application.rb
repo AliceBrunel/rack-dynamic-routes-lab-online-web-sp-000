bb901434ae82c7dbeb4af314acdf2faa5bffa8b4
Alice Brunel <alc.brunel@gmail.com>
2019-09-22 10:03:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       item_name = req.path.split("/items/").last 
       item = @@items.find{|i| i.name == item_name} 
       
-      if item.name? 
+      if item.name == nil
         resp.write item.price
       else 
         resp.write "Item not found"
